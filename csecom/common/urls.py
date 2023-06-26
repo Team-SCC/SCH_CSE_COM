0c7185147cc0dd4c43bd80843109ec577f5dbffe
LeeInGyu <developlee20@gmail.com>
2023-06-26 16:09:34 +0900

    로그인, 로그아웃 기능 구현  완료

--- csecom/common/urls.py
+++ csecom/common/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = "common"
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.log_out, name='logout'),
     path('signup/', views.signup, name='signup'),
 ]
