626998ee88a6ba27803035513295d112c152f8aa
LeeInGyu <developlee20@gmail.com>
2023-06-29 15:39:34 +0900

    기존 locker, User form 제거

--- csecom/csecom/settings.py
+++ csecom/csecom/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'locker.apps.LockerConfig',
     'common.apps.CommonConfig',
 ]
 
