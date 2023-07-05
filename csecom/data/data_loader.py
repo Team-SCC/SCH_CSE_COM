5ce332d971f4574b1ce849d51c582a835f1bc7df
LeeInGyu <developlee20@gmail.com>
2023-07-05 13:53:20 +0900

    data_loader.py 일부 기능 함수화

--- csecom/data/data_loader.py
+++ csecom/data/data_loader.py
@@ -1,16 +1,26 @@
 import os
 import pandas as pd
 
+def get_from_path(path:str):
+    '''주소 참조 후 파일 불러오기
+    공통 부분 함수화
+    '''
+    relative_path = path
+    
+    try:
+        data = pd.read_csv(relative_path)
+    except:
+        absolute_path = os.path.abspath(relative_path)
+        data = pd.read_csv(absolute_path)
+        
+    return data
+
 def student_id_loader(self):
     '''데이터파일 불러와서 학번 리스트 반환
     '''
     relative_path = './data/si_checker.csv'
 
-    try: # 상대경로
-        student_id_data = pd.read_csv(relative_path)
-    except: # 에러시 상대경로를 절대 경로로 변경
-        absolute_path = os.path.abspath(relative_path) 
-        student_id_data = pd.read_csv(absolute_path)
+    student_id_data = get_from_path(relative_path)
 
     student_id_list = []
 
