25bd59e6f66a71408b2156884b30db052333d4db
LeeInGyu <developlee20@gmail.com>
2023-07-17 15:34:12 +0900

    학회비 납부 확인 방식 DB로 변경

--- csecom/locker/admin.py
+++
@@ -1,9 +0,0 @@
-# from django.contrib import admin
-
-# from .models import Locker
-
-# class LockerAdmin(admin.ModelAdmin):
-#     list_display = ('student_id','locker_id')
-#     ordering = ('locker_id',)
-    
-# admin.site.register(Locker, LockerAdmin)
--- csecom/si_checker/views.py
+++ csecom/si_checker/views.py
@@ -3,4 +3,10 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='common:login')
 def si_checker(request):
-    return render(request, 'si_checker.html')
+    
+    user = request.user
+    
+    if user.is_check:
+        return render(request, 'si_checker.html', {'is_check': True})
+    
+    return render(request, 'si_checker.html', {'is_check': False})
