19ff4c4d83eacb7994c96d66768ca67064d19aeb
ParkJaeHyun <hyunihyuni04@gmail.com>
2023-10-25 18:11:40 +0900

    add locker return function

--- csecom/locker/urls.py
+++ csecom/locker/urls.py
@@ -7,4 +7,5 @@ app_name = "locker"
 urlpatterns = [
     path('', views.locker_main, name='locker'),
     path('create', views.locker_create, name='locker_create'),
+    path('return', views.locker_return, name='locker_return'),
 ]
